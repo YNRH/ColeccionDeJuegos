54d2b0f717cca06b53e20c880640e983c894bd4d
ynrh <yesica.rojas@tecsup.edu.pe>
2023-10-29 10:05:26 -0700

    Initial Commit

---
+++ YRojasColeccionDeDatos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YRojasColeccionDeDatos
+//
+//  Created by Yesica Rojas on 10/29/23.
+//  Copyright © 2023 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
